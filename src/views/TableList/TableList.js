acbe71f85913f0e77fdc8b90d0e8351dfea674c5
darenleongzy <darenleongzy@gmail.com>
2021-10-23 16:20:07 +0800

    Added material-dashboard-template

---
+++ src/views/TableList/TableList.js
@@ -0,0 +1,119 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0",
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF",
+    },
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1",
+    },
+  },
+};
+
+const useStyles = makeStyles(styles);
+
+export default function TableList() {
+  const classes = useStyles();
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
+            <p className={classes.cardCategoryWhite}>
+              Here is a subtitle for this table
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={[
+                "Name",
+                "Current Price",
+                "Traded Price",
+                "% Change",
+                "Dividends",
+                "Transaction Date",
+              ]}
+              tableData={[
+                ["GME", "$200.00", "$190.00", "10%", "NA", "10-10-21"],
+                ["BB", "$10.08", "$200.00-Waas", "10%", "NA", "10-10-21"],
+                ["COIN", "$300.90", "$200.00", "10%", "NA", "10-10-21"],
+                ["UWMC", "$7.09", "$10.00", "10%", "NA", "10-10-21"],
+                ["PLTR", "$24.56", "$25.00", "10%", "NA", "10-10-21"],
+                ["SBUX", "$114.3", "$118.50", "10%", "NA", "10-10-21"],
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card plain>
+          <CardHeader plain color="primary">
+            <h4 className={classes.cardTitleWhite}>
+              Table on Plain Background
+            </h4>
+            <p className={classes.cardCategoryWhite}>
+              Here is a subtitle for this table
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["ID", "Name", "Country", "City", "Salary"]}
+              tableData={[
+                ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
+                ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
+                ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
+                [
+                  "4",
+                  "Philip Chaney",
+                  "$38,735",
+                  "Korea, South",
+                  "Overland Park",
+                ],
+                [
+                  "5",
+                  "Doris Greene",
+                  "$63,542",
+                  "Malawi",
+                  "Feldkirchen in Kärnten",
+                ],
+                ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"],
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
+  );
+}
